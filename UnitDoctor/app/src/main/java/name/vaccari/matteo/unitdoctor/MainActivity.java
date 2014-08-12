8e86c833cad747aeb11324e3cb604bd142101fb9
Matteo Vaccari <vaccari@pobox.com>
2014-08-12 12:50:06 +0200

    First stab at integrating UnitDoctor

--- UnitDoctor/app/src/main/java/name/vaccari/matteo/unitdoctor/MainActivity.java
+++ UnitDoctor/app/src/main/java/name/vaccari/matteo/unitdoctor/MainActivity.java
@@ -2,16 +2,29 @@ package name.vaccari.matteo.unitdoctor;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.*;
+import android.widget.TextView;
+
+import name.vaccari.matteo.unitdoctor.core.UnitDoctor;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnKeyListener {
+    private UnitDoctor doctor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView inputNumberField = (TextView) findViewById(R.id.inputNumber);
+        TextView fromUnitField = (TextView) findViewById(R.id.fromUnit);
+        TextView toUnitField = (TextView) findViewById(R.id.toUnit);
+        TextView resultField = (TextView) findViewById(R.id.result);
+        AndroidUnitDoctorView view = new AndroidUnitDoctorView(inputNumberField, fromUnitField, toUnitField, resultField);
+
+        doctor = new UnitDoctor(view);
+
+        inputNumberField.setOnKeyListener(this);
     }
 
 
@@ -32,5 +45,11 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        doctor.onChange();
+        return false;
     }
 }

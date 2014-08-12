70f5d21a136876442e8828dc489cdf96b8eb8543
Matteo Vaccari <vaccari@pobox.com>
2014-08-12 15:01:43 +0200

    refactored directories

--- life/gen/it/xpug/life/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package it.xpug.life;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- life/src/com/thoughtworks/game_of_life/core/Cell.java
+++
@@ -1,6 +0,0 @@
-package com.thoughtworks.game_of_life.core;
-
-public interface Cell {
-    boolean isAlive();
-    boolean willBeAlive(int numberOfAliveNeighbours);
-}

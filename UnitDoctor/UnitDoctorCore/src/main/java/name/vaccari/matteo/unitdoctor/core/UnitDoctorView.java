2ca9892a2adca6efd76671a6f95d4165d5b10175
Matteo Vaccari <vaccari@pobox.com>
2014-08-12 10:49:15 +0200

    I has a falling test

---
+++ UnitDoctor/UnitDoctorCore/src/main/java/name/vaccari/matteo/unitdoctor/core/UnitDoctorView.java
@@ -0,0 +1,8 @@
+package name.vaccari.matteo.unitdoctor.core;
+
+public interface UnitDoctorView {
+    double inputNumber();
+    String fromUnit();
+    String toUnit();
+    void showResult(double result);
+}

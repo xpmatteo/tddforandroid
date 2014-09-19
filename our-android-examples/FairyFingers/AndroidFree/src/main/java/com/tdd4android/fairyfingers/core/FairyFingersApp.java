03cb344ecf059c466df1f75267de99c93b6d79e8
Matteo Vaccari <vaccari@pobox.com>
2014-09-19 13:58:57 +0200

    start session

--- our-android-examples/FairyFingers/AndroidFree/src/main/java/com/tdd4android/fairyfingers/core/FairyFingersApp.java
+++ our-android-examples/FairyFingers/AndroidFree/src/main/java/com/tdd4android/fairyfingers/core/FairyFingersApp.java
@@ -2,9 +2,6 @@ package com.tdd4android.fairyfingers.core;
 
 import java.util.List;
 
-/**
- * Created by matteo on 12/09/14.
- */
 public class FairyFingersApp {
   public FairyFingersApp(PathStorage pathStorage) {
 
